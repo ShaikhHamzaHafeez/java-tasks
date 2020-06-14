6174787d87af8fb7c0ffac23de73f87a02fb8ce3
ShaikhHamzaHafeez <hamzahafeez980@gmail.com>
2020-06-14 07:34:43 -0700

    tasks of java script

---
+++ app.js
@@ -0,0 +1,86 @@
+// ===========================================================
+//                    chapter # 1
+// ===========================================================  
+
+    //  Task # 1
+
+    alert('Welcome to our website');
+
+    // Task # 2
+
+    alert('Error! Please Enter a valid password')
+
+    // Task # 3
+     
+    alert('Welcome to JS Land...\nHappy Coding!')
+
+    // Task # 4
+
+    alert('Welcome to JS Land....')
+    alert('Happy Coding!\nPrevent this page creating additional dailogs')
+
+    // Task # 5
+    console.log(alert('Hello... I can run JS through my web browser console'))
+
+
+
+    // ===========================================================
+//                     chapter # 2
+//  ===========================================================  
+
+
+//  Task # 1
+
+var userName ;
+
+//  Task # 2
+var myName ="Hamza Hafeez";
+
+//  Task # 3
+var message;
+ message = 'Hello World';
+
+alert(message);
+
+//  Task # 4
+
+var name=prompt('Enter your name?');
+var age=prompt('Enter your age?');
+var skill=prompt('Enter your skill?')
+alert(name);
+alert(age);
+alert(skill);
+
+//  Task # 6
+
+var email='hamzahafeez980@gmail.com';
+alert('My email address is '+ email);
+
+//  Task # 7
+
+var book;
+book= 'A smarter way to learn JavaScript'
+alert('I am trying to learn from the book '+ book);
+
+//  Task # 8
+
+document.write('YAH! I can write HTML content through JavaScript.')
+
+//  Task # 9
+
+var design='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(design);
+
+
+// =================================================
+//                 chapter # 3
+// =========================================-========                
+
+// task # 1
+
+var age= 'I am 20 years old';
+alert(age);
+
+// task # 2
+
+
